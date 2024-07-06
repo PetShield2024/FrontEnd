1586e8f3c55f9a5b253f246325b6066a4e282201
bigtr3 <tmddms1002@gmail.com>
2024-07-07 01:15:02 +0900

    [Feat] 비만도 검사 , myinfo 뷰 완료

--- app/src/main/java/com/example/petshield/CameraFragment.kt
+++ app/src/main/java/com/example/petshield/CameraFragment.kt
@@ -1,4 +1,18 @@
 package com.example.petshield
 
-class CameraFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class CameraFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_camera, container, false)
+
+        return view
+    }
 }
