2b6a4625f56bc13b3aa4a3abc7369f720dba4fb4
bigtr3 <tmddms1002@gmail.com>
2024-07-16 21:45:32 +0900

    [Feat]사료 추천 화면

--- app/src/main/java/com/example/petshield/FilterFragment.kt
+++ app/src/main/java/com/example/petshield/FilterFragment.kt
@@ -1,25 +1,18 @@
 package com.example.petshield
 
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-
-class CameraFragment : Fragment() {
-
-    private val REQUEST_IMAGE_CAPTURE = 672
-    private val imageFilePath: String? = null
-    private val photoUri: Uri? = null
+class FilterFragment : Fragment()  {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_camera, container, false)
+        val view = inflater.inflate(R.layout.fragment_filter, container, false)
 
         return view
     }
-
 }
